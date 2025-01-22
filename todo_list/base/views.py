39415a022121facaac703038ac18672ff1364208
tkDev <tkdev0101@gmail.com>
2025-01-22 09:28:42 +0200

    Created 1st  Model and Migration

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from.models import Task
+
 
 # Create your views here.
+def taskList(request):
+    return HttpResponse('To Do List')
