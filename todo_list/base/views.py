9e9f2321d2c8a5b7e17672624eefd5c7f0695af4
tkDev <tkdev0101@gmail.com>
2025-01-22 11:05:45 +0200

    Updated Views and created Templates
    Created Class based Views - TaskList, TaskDetail
    Created Templates for base app

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,8 +1,17 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 from.models import Task
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-# Create your views here.
-def taskList(request):
-    return HttpResponse('To Do List')
+class TaskList(ListView): #task_list.html
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView): #task.html
+    model = Task
+    context_object_name = 'task_title'
+    template_name= 'base/task.html'
+    
