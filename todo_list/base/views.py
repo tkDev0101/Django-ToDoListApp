3c6fa09a79b612e651cda85c7ed2c582ebfbbece
tkDev <tkdev0101@gmail.com>
2025-01-27 10:52:26 +0200

    User Specific Data
    User Specific Data - data being displayed is specific to each user.
    
    get_context_data()

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
+#Login View
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
@@ -19,28 +20,48 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tasks')
 
 
+#View Taskss
 class TaskList(LoginRequiredMixin, ListView): #task_list.html
     model = Task
     context_object_name = 'tasks'
 
+    #User only sees their specific data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] =  context['tasks'].filter(user= self.request.user)
+        context['count'] =  context['tasks'].filter(complete=False).count()
+        return context
+        
 
+
+#Details of Task
 class TaskDetail(LoginRequiredMixin, DetailView): #task.html
     model = Task
     context_object_name = 'task_title'
     template_name= 'base/task.html'
     
 
-class TaskCreate(LoginRequiredMixin, CreateView):
+#Create Task
+class TaskCreate(LoginRequiredMixin, CreateView): #task_form.html
     model = Task
-    fields = '__all__'
+    fields = {'title', 'description', 'complete'}
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+    
+    
 
+
+#Update Task
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = {'title', 'description', 'complete'}
     success_url = reverse_lazy('tasks')
 
+
+#Delete Task
 class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
